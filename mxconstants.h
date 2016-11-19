083dd3ad2d459ba851d37cfefbe249c32526e76a
BugAngel <1055056138@qq.com>
2016-11-19 22:22:01 +0800

    红外漫反射扫描板

---
+++ mxconstants.h
@@ -0,0 +1,93 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MXCONSTANT_H
+#define __MXCONSTANT_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define KEY1_Pin GPIO_PIN_2
+#define KEY1_GPIO_Port GPIOB
+#define KEY8_Pin GPIO_PIN_11
+#define KEY8_GPIO_Port GPIOB
+#define KEY10_Pin GPIO_PIN_12
+#define KEY10_GPIO_Port GPIOB
+#define KEY7_Pin GPIO_PIN_13
+#define KEY7_GPIO_Port GPIOB
+#define KEY9_Pin GPIO_PIN_14
+#define KEY9_GPIO_Port GPIOB
+#define KEY6_Pin GPIO_PIN_15
+#define KEY6_GPIO_Port GPIOB
+#define KEY11_Pin GPIO_PIN_6
+#define KEY11_GPIO_Port GPIOC
+#define KEY12_Pin GPIO_PIN_7
+#define KEY12_GPIO_Port GPIOC
+#define KEY14_Pin GPIO_PIN_8
+#define KEY14_GPIO_Port GPIOC
+#define KEY13_Pin GPIO_PIN_9
+#define KEY13_GPIO_Port GPIOC
+#define KEY16_Pin GPIO_PIN_8
+#define KEY16_GPIO_Port GPIOA
+#define KEY15_Pin GPIO_PIN_11
+#define KEY15_GPIO_Port GPIOA
+#define KEY17_Pin GPIO_PIN_12
+#define KEY17_GPIO_Port GPIOA
+#define KEY18_Pin GPIO_PIN_15
+#define KEY18_GPIO_Port GPIOA
+#define KEY2_Pin GPIO_PIN_3
+#define KEY2_GPIO_Port GPIOB
+#define KEY3_Pin GPIO_PIN_4
+#define KEY3_GPIO_Port GPIOB
+#define KEY4_Pin GPIO_PIN_7
+#define KEY4_GPIO_Port GPIOB
+#define KEY5_Pin GPIO_PIN_9
+#define KEY5_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MXCONSTANT_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
